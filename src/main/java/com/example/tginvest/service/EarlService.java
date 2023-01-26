38e1ecf2f6c0609497796318f1ac9131fb8ab0b5
lifefire1 <firend228@mail.ru>
2023-01-26 14:51:03 +0300

    -added the ability to view the graph of the dollar in png per day with an interval of an hour

---
+++ src/main/java/com/example/tginvest/service/EarlService.java
@@ -0,0 +1,82 @@
+package com.example.tginvest.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.Hour;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.springframework.stereotype.Service;
+import ru.tinkoff.piapi.contract.v1.CandleInterval;
+import ru.tinkoff.piapi.contract.v1.HistoricCandle;
+import ru.tinkoff.piapi.core.InvestApi;
+import ru.tinkoff.piapi.core.utils.MapperUtils;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Service
+@Slf4j
+public class EarlService {
+    private static final String token = "t.CLxPGguplDzK79-qlB_0MjkzBLCEzgJ9r8EJiVdUkIdGX1VrYlk21Fls4ulfubV6oj9kTc8ieVdOkjaOGCBPHA";
+
+    private static final InvestApi readOnlyToken = InvestApi.createReadonly(token);
+
+    private static DecimalFormat decimalFormat = new DecimalFormat("###.###");
+    private static Instant instant1 = Clock.system(ZoneId.of("Europe/Moscow")).instant();
+
+    private static Instant instant2 = instant1.minus(1, ChronoUnit.DAYS);
+
+    public TimeSeriesCollection createImageEarl () throws ExecutionException, InterruptedException {
+        TimeSeries s1 = new TimeSeries("график доллара");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        var market = readOnlyToken
+                .getMarketDataService()
+                .getCandles(
+                        "a22a1263-8e1b-4546-a1aa-416463f104d3"
+                        ,instant2
+                        ,instant1
+                        ,CandleInterval.CANDLE_INTERVAL_HOUR);
+        market.get().forEach(e -> {
+
+            Date myDate = Date.from(Instant.ofEpochSecond(e.getTime().getSeconds()));
+            Day day = new Day(myDate.getDay(),myDate.getMonth() + 1, myDate.getYear() + 1900);
+            s1.add(new Hour(myDate.getHours(),day), MapperUtils.quotationToBigDecimal(e.getClose()));
+        });
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(s1);
+
+        return dataset;
+    }
+
+
+    public File getGraph () throws ExecutionException, InterruptedException, IOException {
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "График доллара",
+                "",
+                "",
+                createImageEarl(),
+                true,
+                true,
+                false
+        );
+        int width = 800;
+        int height = 800;
+        File res = new File("res.png");
+        ChartUtilities.saveChartAsPNG(res,chart,width,height);
+        return res;
+    }
+}
